7bb094ae1631748011ef14bc0d331bce2ec42bdd
Taranko Taras <tarastarnko2@gmail.com>
2023-08-09 22:05:53 +0300

    Fixed all animations

--- TarankoWebSite/wwwroot/js/home.js
+++ TarankoWebSite/wwwroot/js/home.js
@@ -1,5 +1,6 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     const animatedElements = document.querySelectorAll(".appearance-annimation");
+    const hoverElements = document.querySelectorAll(".hover-div");
 
     animatedElements.forEach(function (element, index) {
         element.style.transitionDelay = `${index * 0.5}s`; // Затримка залежно від індексу
@@ -10,9 +11,49 @@
             if (index === animatedElements.length - 1) {
                 animatedElements.forEach(function (el) {
                     el.style.transitionDelay = "0s"; // Зменшення затримки після анімації\
-                    el.classList.add("hover-annimation");
+                    
+                    
                 });
             }
         });
     });
+    hoverElements.forEach(function (element) {
+        element.classList.add("hover-annimation");
+        element.addEventListener("mouseover", function () {
+            changeText(element);
+        });
+        element.addEventListener("mouseout", function () {
+            resetText(element);
+        });
+    });
 });
+function changeText(el) {
+    var link = el.querySelector("a");
+    if (link.innerText === "Hello world.") {
+        link.innerText = "About Me";
+    }
+    else if (link.innerText === "I am") {
+        link.innerText = "Resume";
+    }
+    else if (link.innerText === "Taras Taranko") {
+        link.innerText = "Projects";
+    }
+    else {
+        link.innerText = "Contacts";   
+    }  
+}
+function resetText(el) {
+    var link = el.querySelector("a");
+    if (link.innerText === "About Me") {
+        link.innerText = "Hello world.";
+    }
+    else if (link.innerText === "Resume") {
+        link.innerText = "I am";
+    }
+    else if (link.innerText === "Projects") {
+        link.innerText = "Taras Taranko";
+    }
+    else {
+        link.innerText = ".NET developer";
+    }
+}
