b2d052330734206e46fd14ac9dbccbb813517cb4
Taranko Taras <tarastarnko2@gmail.com>
2023-08-09 21:08:42 +0300

    Fixed appearence animation and adding transition for hover

--- TarankoWebSite/wwwroot/js/home.js
+++ TarankoWebSite/wwwroot/js/home.js
@@ -1,8 +1,18 @@
-﻿document.addEventListener("DOMContentLoaded", function() {
+﻿document.addEventListener("DOMContentLoaded", function () {
     const animatedElements = document.querySelectorAll(".appearance-annimation");
 
-animatedElements.forEach(function(element, index) {
-    element.style.transitionDelay = `${index * 0.5}s`; // Затримка залежно від індексу
-element.classList.add("appearance");
+    animatedElements.forEach(function (element, index) {
+        element.style.transitionDelay = `${index * 0.5}s`; // Затримка залежно від індексу
+        element.classList.add("appearance");
+
+        // Очікування завершення анімації для оновлення затримки
+        element.addEventListener("transitionend", function () {
+            if (index === animatedElements.length - 1) {
+                animatedElements.forEach(function (el) {
+                    el.style.transitionDelay = "0s"; // Зменшення затримки після анімації\
+                    el.classList.add("hover-annimation");
+                });
+            }
+        });
     });
 });
