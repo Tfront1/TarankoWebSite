4919a5285cbc84a94322d042b055fce7baaf06fe
Taranko Taras <tarastarnko2@gmail.com>
2023-08-09 16:13:12 +0300

    Aded apearance animation for home page text

---
+++ TarankoWebSite/wwwroot/js/home.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+    const animatedElements = document.querySelectorAll(".appearance-annimation");
+
+animatedElements.forEach(function(element, index) {
+    element.style.transitionDelay = `${index * 0.5}s`; // Затримка залежно від індексу
+element.classList.add("appearance");
+    });
+});
