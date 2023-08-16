4df23351a1afd27929541acf6b19c71a67748d1a
Taranko Taras <tarastarnko2@gmail.com>
2023-08-17 00:43:55 +0300

    Aded ability to download cv in resume page

--- TarankoWebSite/wwwroot/js/resume.js
+++ TarankoWebSite/wwwroot/js/resume.js
@@ -1,3 +1,13 @@
 ﻿window.addEventListener("load", function () {
     document.body.style.overflow = "visible";
+    const hoverElements = document.querySelectorAll("#resume--hover-div");
+    hoverElements.forEach(function (elm) {
+
+        elm.addEventListener("mouseover", function () {
+            elm.classList.add("resume--hover-annimation");
+        });
+        elm.addEventListener("mouseout", function () {
+            elm.classList.add("resume--hover-annimation");
+        });
+    });
 });
