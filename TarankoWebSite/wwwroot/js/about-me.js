59cb4696d0d3410b15baba538c039c1bc301b9af
Taranko Taras <tarastarnko2@gmail.com>
2023-08-16 19:26:24 +0300

    Done contacts page

--- TarankoWebSite/wwwroot/js/about-me.js
+++ TarankoWebSite/wwwroot/js/about-me.js
@@ -1,29 +1,3 @@
 ﻿window.addEventListener("load", function () {
     document.body.style.overflow = "visible";
-    const hoverElements = document.querySelectorAll("#hover-div");
-    hoverElements.forEach(function (elm) {
-
-        elm.addEventListener("mouseover", function () {
-            elm.classList.add("hover-annimation");
-            changeText(elm);
-        });
-        elm.addEventListener("mouseout", function () {
-            elm.classList.add("hover-annimation");
-            resetText(elm);
-        });
-    });
 });
-function changeText(el) {
-    var link = el.querySelector("a");
-    if (link.innerText === "tarankotaras.net@gmail.com") {
-        link.innerText = "Copy to clipboard";
-    }
-    link.style.pointerEvents = 'auto';
-}
-function resetText(el) {
-    var link = el.querySelector("a");
-    if (link.innerText === "Copy to clipboard") {
-        link.innerText = "tarankotaras.net@gmail.com"";
-    }
-    link.style.pointerEvents = 'auto';
-}
--- TarankoWebSite/wwwroot/js/contacts.js
+++ TarankoWebSite/wwwroot/js/contacts.js
@@ -1,3 +1,36 @@
 ﻿window.addEventListener("load", function () {
+    var copyLink = document.getElementById('copyLink');
     document.body.style.overflow = "visible";
+    const hoverElements = document.querySelectorAll("#contacts--hover-div");
+    hoverElements.forEach(function (elm) {
+
+        elm.addEventListener("mouseover", function () {
+            elm.classList.add("contacts--hover-annimation");
+            changeText(elm);
+        });
+        elm.addEventListener("mouseout", function () {
+            elm.classList.add("contacts--hover-annimation");
+            resetText(elm);
+        });
+    });
+    copyLink.addEventListener('click', function (event) {
+        event.preventDefault(); // Перешкоджає переходу за посиланням
+
+        const textToCopy = 'tarankotaras.net@gmail.com';
+        navigator.clipboard.writeText(textToCopy)
+    });
 });
+function changeText(el) {
+    var link = el.querySelector("a");
+    if (link.innerText === "tarankotaras.net@gmail.com") {
+        link.innerText = "Copy to clipboard";
+    }
+    link.style.pointerEvents = 'auto';
+}
+function resetText(el) {
+    var link = el.querySelector("a");
+    if (link.innerText === "Copy to clipboard") {
+        link.innerText = "tarankotaras.net@gmail.com";
+    }
+    link.style.pointerEvents = 'auto';
+}
